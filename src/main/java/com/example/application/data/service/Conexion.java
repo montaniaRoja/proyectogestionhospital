1c2ae5c482802bf127d4171c612962f0d5a73c0d
adolfo <adolfocp1972@gmail.com>
2023-11-01 16:06:36 -0600

    se agrego alerta para encender el servidor

--- src/main/java/com/example/application/data/service/Conexion.java
+++ src/main/java/com/example/application/data/service/Conexion.java
@@ -11,7 +11,7 @@ public class Conexion {
 	// Luego, puedes establecer la conexión
 
 
-	
+	//conexion a google cloud, si no esta en linea favor solicitar reinicio.
 	private final static String ruta = "jdbc:postgresql://34.71.178.20:5432/hospitaldb";
 	private final static String usuario = "adolfo";
 	private final static String contrasenia = "ofloda01";
@@ -30,7 +30,7 @@ public class Conexion {
 			return true;
 		} catch (SQLException ex) {
 			// TODO Auto-generated catch block
-			JOptionPane.showMessageDialog(null,"Conexion Fallo");
+			JOptionPane.showMessageDialog(null,"Favor verificar si servidor esta encendido");
 			return false;
 		}
 		
