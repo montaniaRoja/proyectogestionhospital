40baabf72b5cee927aec862bbb0eecc6c2f3565d
montaniaRoja <adolfocp1972@gmail.com>
2023-11-10 17:59:56 -0600

    conexion a retrofit

--- src/main/java/com/example/application/model/DatabaseRepository.java
+++ src/main/java/com/example/application/model/DatabaseRepository.java
@@ -1,5 +1,18 @@
 package com.example.application.model;
 
-public class DatabaseRepository {
+import com.example.application.data.PacientesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+
+public interface DatabaseRepository {
+	
+	@Headers({
+	    "Accept: application/json",
+	    "User-Agent: Retrofit-Sample-App"
+	})
+	@GET("/pls/apex/adolfouth/hospital/pacientes")
+	Call<PacientesResponse> consultarPacientes();
 
 }
--- src/main/java/com/example/application/model/DatabaseRepositoryImpl.java
+++ src/main/java/com/example/application/model/DatabaseRepositoryImpl.java
@@ -1,5 +1,40 @@
 package com.example.application.model;
 
-public class DatabaseRepositoryImpl {
+import java.io.IOException;
 
+import com.example.application.data.PacientesResponse;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
+public class DatabaseRepositoryImpl {
+	
+	private static DatabaseRepositoryImpl INSTANCE;
+	private static DatabaseClient client;
+	
+	private DatabaseRepositoryImpl(String url, Long timeout) {
+		client=new DatabaseClient(url, timeout);
+	}
+	
+	public static DatabaseRepositoryImpl getInstance(String url, Long timeout) {
+		if(INSTANCE==null) {
+			synchronized (DatabaseRepositoryImpl.class) {
+				if(INSTANCE==null) {
+					INSTANCE=new DatabaseRepositoryImpl(url, timeout);
+				}
+			}
+		}
+		return INSTANCE;
+	}
+	
+	public PacientesResponse consultarPacientes() throws IOException {
+		Call<PacientesResponse> call=client.getDatabase().consultarPacientes();
+		Response<PacientesResponse> response=call.execute();
+		if(response.isSuccessful()) {
+			return response.body();
+		}else {
+			return null;
+		}
+	}
+	
 }
